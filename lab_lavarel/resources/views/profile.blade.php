8857851f7c783f2c2ff2a3d73a0739960410adb4
dominhquan <dominhquan.uit@gmail.com>
2015-04-23 23:46:57 +0700

    update

--- lab_lavarel/resources/views/profile.blade.php
+++ lab_lavarel/resources/views/profile.blade.php
@@ -29,25 +29,72 @@
                 <!-- /.panel-body -->
             </div>
         </div>
-        <script src="{{ asset('/assets/jquery/jquery-1.10.2.min.js')}}"></script>
-        <link href="{{ asset('/assets/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
-        <script src="{{ asset('/assets/bootstrap/js/bootstrap.min.js')}}"></script>
-        <script src="{{ asset('/assets/prettify/run_prettify.js')}}"></script>
-        <link href="{{ asset('/assets/bootstrap-dialog/css/bootstrap-dialog.min.css')}}" rel="stylesheet" type="text/css" />
-        <script src="{{ asset('/assets/bootstrap-dialog/js/bootstrap-dialog.min.js')}}"></script>
+        <link href="{{ asset('/js/bootstrap-dialog/css/bootstrap-dialog.css')}}" rel="stylesheet" type="text/css" />
+        <script src="{{ asset('/js/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ asset('/js/dataTables.bootstrap.js') }}"></script>
+        <script src="{{ asset('/js/notify.min.js') }}"></script>
+        <script src="{{ asset('/js/bootstrap-dialog/js/bootstrap-dialog.js')}} "></script>
         <script type="text/javascript">
+            var token="{{ Session::token() }}";
             function show(){
+                var data = '<option value="999"selected="selected">Admin</option><option value="0"  >Normal</option></select>';
+                if('{{$role}}'!="Admin"){
+                    data = '<option value="999">Admin</option><option value="0"  selected="selected">Normal</option></select>';
+                }
                 BootstrapDialog.show({
-                    title: 'Button Hotkey',
-                    message: $('<textarea class="form-control" placeholder="Try to input multiple lines here..."></textarea>'),
+                    size:BootstrapDialog.SIZE_WIDE,
+                    title: 'Update user',
+                    message: '' +
+                    '</br> <label>Name:</label> <input id="name_create" value="{{ $name }}" type="text" class="form-control" required> ' +
+                    '</br> <label>Email :</label> <input id="email_create" value="{{ $email}}" type="text" class="form-control">'+
+                    '</br> <label>Password :</label> <input id="password_create" type="password" class="form-control">'+
+                    '</br> <label>Role :</label> <select id="role_create" class="form-control">' +data,
                     buttons: [{
-                        label: '(Enter) Button A',
-                        cssClass: 'btn-primary',
-                        hotkey: 13, // Enter.
-                        action: function() {
-                            alert('You pressed Enter.');
+                        autospin: true,
+                        icon: 'glyphicon glyphicon-send',
+                        label: 'Save',
+                        cssClass: 'btn-success',
+                        draggable:true,
+                        action: function(dialog) {
+                            $.ajax({
+                                url : '{{ url('user/edit') }}',
+                                type : 'POST',
+                                data : {
+                                    _token: token,
+                                    "id" : '{{$id}}',
+                                    "name" : dialog.getModalContent().find("#name_create").val(),
+                                    "email" : dialog.getModalContent().find("#email_create").val(),
+                                    "role" : dialog.getModalContent().find("#role_create").val(),
+                                    "password" : dialog.getModalContent().find("#password_create").val()
+                                },
+                                success : function(data){
+                                    data = JSON.parse(data);
+                                    if(data.result=="ok"){
+                                        dialog.close();
+                                        updateNotification('Thêm thành công :');
+                                        location.reload();
+                                    }else{
+                                        updateNotification('Thêm thất bại','error');
+                                    }
+                                }
+                            });
+                        }
+                    },{
+                        label: 'Đóng',
+                        cssClass: 'btn-warning',
+                        action: function(dialog) {
+                            dialog.close();
                         }
                     }]
+                });
+            }
+            function updateNotification(data,type){
+                type = typeof type !== 'undefined' ? type : 'success';
+                $.notify( data,{
+                    position:"top center",
+                    autoHide:true,
+                    autoHideDelay:3000,
+                    className:type
                 });
             }
         </script>
